5fee0d74f5abc19667588a9c331908ea46b72a8f
Pratyush Nandi <32712521+pratyush48@users.noreply.github.com>
2020-10-23 12:52:36 +0530

    Update first.s

--- first.s
+++ first.s
@@ -6,7 +6,7 @@ __main  FUNCTION
 		 MOV r5,#100;
        	 VMOV.F32 s4,#-1;
 		 VMOV.F32 s5,#1;
-		 VMOV.F32 s7,#-0.5; 
+		 VMOV.F32 s10,#-0.5; 
          VMOV.F32 s0,#1;
 		 VMOV.F32 s1,#1;
 		 VMOV.F32 s2,#1;
@@ -14,7 +14,7 @@ __main  FUNCTION
 		 
 loop	 CMP  r0,r5;
          VDIV.F32 s3,s0,s1; denominator
-		 VMUL.F32 s5,s5,s7; power
+		 VMUL.F32 s5,s5,s10; power
 		 VFMA.F32 s6,s3,s5; s6 stores the accumulated value
 		 VADD.F32 s1,s1,s2; incrementer 
 		 VMUL.F32 s0,s0,s4; sign change
