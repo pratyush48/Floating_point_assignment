9f6567c3b7c2d2c0787b4f8c5e52454a3b49c9c1
Pratyush Nandi <32712521+pratyush48@users.noreply.github.com>
2020-10-23 12:44:45 +0530

    Update first.s

--- first.s
+++ first.s
@@ -1,13 +1,26 @@
-     area     appcode, CODE, READONLY
-     export __main
+     AREA     appcode, CODE, READONLY
+     EXPORT __main
 	 ENTRY 
-__main  function		 
-       VLDR.F32   s4, = 0
-       VLDR.F32   s5, = 0 
-       VDIV.F32    s6,s5,s4
-       VLDR.F32   s5, = -1
-       VSQRT.F32 S7, S5	   
-		MOV 
+__main  FUNCTION		         
+		 MOV r0,#1;
+		 MOV r5,#100;
+       	 VMOV.F32 s4,#-1;
+		 VMOV.F32 s5,#1;
+		 VMOV.F32 s7,#-0.5; 
+         VMOV.F32 s0,#1;
+		 VMOV.F32 s1,#1;
+		 VMOV.F32 s2,#1;
+
+		 
+loop	 CMP  r0,r5;
+         VDIV.F32 s3,s0,s1; denominator
+		 VMUL.F32 s5,s5,s7; power
+		 VFMA.F32 s6,s3,s5; s6 stores the accumulated value
+		 VADD.F32 s1,s1,s2; incrementer 
+		 VMUL.F32 s0,s0,s4; sign change
+         ADDLT r0,#1;		
+	     BNE   loop
 stop    B stop ; stop program
-endfunc
-     end
+		 
+     ENDFUNC
+     END
